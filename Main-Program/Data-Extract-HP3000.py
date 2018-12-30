73a1ae30fb3ef866dce98b7cf8602a61b3f8a1fb
Thomas <thomas.frivold@gmail.com>
2018-12-30 14:23:13 +0100

    adding all the project files

---
+++ Main-Program/Data-Extract-HP3000.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python2
+# Courtesy of Olivier Guyotot
+# Source: https://www.mail-archive.com/weewx-user@googlegroups.com/msg09026.html
+#
+#[weewx-user] Re: WS-3000
+#olivier . guyotot Sun, 14 Jan 2018 03:57:07 -0800
+#Hi again,
+#
+#I did some more troubleshooting, and I now have the feeling that the driver 
+#isn't doing exactly what it should.
+#
+#First of all, a disclaimer: I am no developer and I know next to nothing 
+#about the USB protocol, so please don't be too harsh on me if what I am 
+#saying is complete BS.
+#
+#Now, I wrote a small python script to try to read data from the WS-3000 and 
+#I actually managed to get this working fine. So I can safely confirm that 
+#the problem doesn't come from the hardware or from the host. The setup I am 
+#using is working fine, the issue must be coming from weewx or the HP3000 
+#driver.
+#
+#According to the output from Wireshark and from what I've read in the 
+#driver's code, I *believe* that there are two issues:
+#1. one is that the driver is sending commands using the 'control' channel.
+#2. probably not directly related to the error I am seeing, but I don't 
+#understand the point of sending all the 0x41, 0x06, 0x08, 0x09, 0x05, 0x03, 
+#0x04 commands. The only interesting ones are 0x03 (current data) and 0x05 
+#(calibration, assuming it must be taken into account to get the 'corrected' 
+#temperature).
+
+#You can find the small script I wrote attached. The only problem I have is 
+#that I have to unplug and replug the station between each run, I probably 
+#don't reset everything properly.
+#You can easily compare the output when sending each of the 0x41, 0x06, 
+#0x08, 0x09, 0x05, 0x03, 0x04 commands, or when sending only 0x03 commands. 
+#By the way, this also means that contrary to what is mentioned in the 
+#driver, the correct way to distinguish between current data and calibration 
+#data is to keep track of which command was sent...
+#
+#Please let me know if this makes sense or not. I am willing to do any 
+#additional test you need in order to get this working correctly.
+#
+#Thanks a lot.
+#
+#-- 
+#You received this message because you are subscribed to the Google Groups 
+#"weewx-user" group.
+#To unsubscribe from this group and stop receiving emails from it, send an email 
+#to weewx-user+unsubscr...@googlegroups.com.
+#For more options, visit https://groups.google.com/d/optout.
+#
+#
+
+# Changelog
+# v1 Original script. Written for Python2 by original author Olivier Guyotot
+# v2 Tiny tweaks to make the script run with Python3. Mainly around arguments to print. 28.12.2018 Thomas Frivold
+# v3 Added numpy arrays and Pandas Dataframes 28.12.2018 Thomas Frivold
+
+import sys, traceback
+import numpy as np
+import pandas as pd
+import usb.core, usb.util
+from time import sleep
+
+def trimbuffer(buf):
+    i = 0
+    while i < len(buf):
+        if (i > 0 and buf[i] == 0x7d and buf[i-1] == 0x40):
+            break
+        i += 1
+    return buf[0:i+1]
+
+def ws3000_write(command):
+    print("ws3000_write: entry")
+    VENDOR = 0x0483
+    PRODUCT = 0x5750
+    CONFIGURATION = 0
+
+    try:
+
+        print ("Detecting device...")
+        device = usb.core.find(idVendor=VENDOR, idProduct=PRODUCT)
+        if device is None:
+            print("Is the WS-3000 connected?")
+            sys.exit(1)
+        print("Device found")
+
+        # Device busy if managed by the kernel
+        if device.is_kernel_driver_active(CONFIGURATION):
+            print("Detaching kernel driver")
+            device.detach_kernel_driver(CONFIGURATION)
+
+        # WS-3000 has only one configuration
+        device.set_configuration()
+
+        # control
+        print ("Sending control packet")
+        #device.ctrl_transfer(0x21, 10, 0, 0, None)
+        device.ctrl_transfer(0, 9, 1, 0, None)
+
+#        commands = [0x41, 0x06, 0x08, 0x09, 0x05, 0x03, 0x04]
+        commands = [0x03]
+        for command in commands:
+            request = [0x7b, command, 0x40, 0x7d]
+            padding = [0x0] * 60
+            request.extend(padding)
+            print ("Sending command: "), (request)
+            device.write(0x01, request, 100)
+            data = device.read(0x82, 64, 100)
+#            data = np.array(device.read(0x82, 62, 100))
+            data = trimbuffer(data)
+            print (data)
+            print ("Array size is",(len(data)))
+            print ("Array element 0", (data[0]))
+            print ("Array element 1", (data[1]))
+# Creating a numpy array - that's better
+            np.array(data)
+# Blasting this into a Pandas Dataframe
+#            dates = pd.date_range('20130101', periods=6)
+#            df = pd.DataFrame(data[0])
+            idx = ['Winter', 'Spring', 'Summer', 'Fall']
+            cols = np.arange(2015,2055,5)
+            df = pd.DataFrame(data.T, index=idx, columns=cols)
+#            df(head)
+#            data = pd.Dataframe(data)
+#            data = []
+#            df = pd.DataFrame(eval(data))
+##            df = df.transpose()
+#            df.columns = ["Team", "Player", "Salary", "Role"]
+#            df(head)
+####### Unnecessary debugging
+#             print type(data)
+#            print len(data)
+#            for value in data:
+#                print hex(value)
+#            sleep(1)
+#
+            for value in data:
+                print ("Type is", type(value))
+#######
+    except:
+        print ("An error occurred:"), sys.exc_info()
+        traceback.print_exc(file=sys.stdout)
+
+    usb.util.dispose_resources(device)
+
+print("Starting tests...")
+ws3000_write("")
+print("End of transmission")
